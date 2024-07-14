f3826ed6e1632d587cb563f6c43bb8b2f2b5b73e
daichi0525 <dai.0525n@gmail.com>
2024-07-14 10:55:35 +0900

    useState複数対処法

--- section1-react/src/components/TestComponent.js
+++ section1-react/src/components/TestComponent.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 
 const TestComponent = () => {
 	const [count, setCount] = useState(0);
+	const onClickChange = () => {
+		setCount((prev)=> prev +1);
+		setCount((prev)=> prev + -3);
+	}
   return (
 	<div>
 		<p>テストコンポーネント</p>
-		<button onClick={() => setCount(count +1)}>+1ボタン</button>
+		<button onClick={onClickChange}>+1ボタン</button>
 		<h2>{count}</h2>
 	</div>
   )
