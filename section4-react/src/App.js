0217a3a6d91cdbb089fb2b97c1328f6a13129c3d
daichi0525 <dai.0525n@gmail.com>
2024-07-31 02:40:34 +0900

    とりあえず画面遷移使えるように

--- section4-react/src/App.js
+++ section4-react/src/App.js
@@ -1,24 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Login from './components/pages/Login';
+import LoginFailed from './components/pages/LoginFailed';
+import NotFound from './components/pages/NotFound';
+import Register from './components/pages/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/loginfailed" element={<LoginFailed />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
